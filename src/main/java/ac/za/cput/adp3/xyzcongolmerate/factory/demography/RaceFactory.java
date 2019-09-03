b55649c63b84e2ee2ebbeb01c14f9b029c628478
MieshkaDollie <mieshkadollie123@gmail.com>
2019-09-03 22:54:26 +0200

    Last commit. Everything done and tested.

--- src/main/java/ac/za/cput/adp3/xyzcongolmerate/factory/demography/RaceFactory.java
+++ src/main/java/ac/za/cput/adp3/xyzcongolmerate/factory/demography/RaceFactory.java
@@ -5,7 +5,7 @@ import ac.za.cput.adp3.xyzcongolmerate.util.Helper;
 
 public class RaceFactory {
 
-    private static final String SUFFIX = Helper.getSuffixFromClassName(RaceFactory.class); // Expecting RF from (R)ace(F)actory.
+    private static final String SUFFIX = Helper.getSuffixFromClassName(RaceFactory.class); // Expecting RF from (R)ace(F)actory
 
     public static Race buildRace(String raceDescription) {
         return new Race.Builder()
